024314c384bcd4b2b6ac2228d3851113996f8828
Guilherme Sperb <guilherme@a52.com.br>
2019-12-03 09:10:35 -0300

    add stop_on_exception

--- timeloop/job.py
+++ timeloop/job.py
@@ -9,6 +9,7 @@ class Job(Thread):
         self.execute = execute
         self.args = args
         self.kwargs = kwargs
+        self.stop_on_exception = False
 
     def stop(self):
         self.stopped.set()
@@ -16,4 +17,8 @@ class Job(Thread):
 
     def run(self):
         while not self.stopped.wait(self.interval.total_seconds()):
-            self.execute(*self.args, **self.kwargs)
+            try:
+                self.execute(*self.args, **self.kwargs)
+            except:
+                if self.stop_on_exception:
+                    break
