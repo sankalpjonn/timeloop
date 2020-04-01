5577c3568a27c4b7ac58c51a2627da95f216ff79
John Ruiz <jruiz@johnruiz.com>
2020-04-01 16:54:08 -0400

    allow jobs to run upon start as well

--- timeloop/app.py
+++ timeloop/app.py
@@ -20,8 +20,8 @@ class Timeloop():
         logger.setLevel(logging.INFO)
         self.logger = logger
 
-    def _add_job(self, func, interval, *args, **kwargs):
-        j = Job(interval, func, *args, **kwargs)
+    def _add_job(self, func, interval, init_on_start, *args, **kwargs):
+        j = Job(interval, init_on_start, func, *args, **kwargs)
         self.jobs.append(j)
 
     def _block_main_thread(self):
@@ -46,9 +46,9 @@ class Timeloop():
             self.logger.info("Stopping job {}".format(j.execute))
             j.stop()
 
-    def job(self, interval):
+    def job(self, interval, init_on_start=False):
         def decorator(f):
-            self._add_job(f, interval)
+            self._add_job(f, interval, init_on_start)
             return f
         return decorator
 
--- timeloop/job.py
+++ timeloop/job.py
@@ -1,11 +1,13 @@
 from threading import Thread, Event
 from datetime import timedelta
 
+
 class Job(Thread):
-    def __init__(self, interval, execute, *args, **kwargs):
+    def __init__(self, interval, init_on_start, execute, *args, **kwargs):
         Thread.__init__(self)
         self.stopped = Event()
         self.interval = interval
+        self.init_on_start = init_on_start
         self.execute = execute
         self.args = args
         self.kwargs = kwargs
@@ -15,5 +17,8 @@ class Job(Thread):
         self.join()
 
     def run(self):
+        if self.init_on_start:
+            self.execute(*self.args, **self.kwargs)
+
         while not self.stopped.wait(self.interval.total_seconds()):
             self.execute(*self.args, **self.kwargs)
